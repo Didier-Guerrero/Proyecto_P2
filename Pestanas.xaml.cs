fdea104cdc9ecf9195e034a1857e542e5a4baf0b
Didier Guerrero <didier.guerrero@udla.ede.ec>
2023-11-26 11:46:19 -0500

    Creacion de la clase Habitacion para posterior uso, modificacion en clase Pestanas para asi añadir la segunda pestaña del programa el cual mostrara la lista de habitaciones, cambios en codigo de Pestanas.xaml.cs que permitira crear la lista en la cual se quemaran los datos de las habitaciones.

--- Pestanas.xaml.cs
+++ Pestanas.xaml.cs
@@ -5,5 +5,13 @@ public partial class Pestanas : TabbedPage
 	public Pestanas()
 	{
 		InitializeComponent();
-	}
+        List<Habitacion> habitaciones = new List<Habitacion>
+        {
+            new Habitacion {Numero = 1, Tipo = "Habitacion doble" },
+            new Habitacion {Numero = 2, Tipo = "Habitación triple"},
+            new Habitacion {Numero = 3, Tipo = "Habitación matrimonial"}
+        };
+
+        habitacionesListView.ItemsSource = habitaciones;
+    }
 }
